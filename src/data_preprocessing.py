4e6fe91335fca24d8e135df8526100d8e238b89a
Brymahh <eseoghenabraimah@gmail.com>
2026-02-26 04:57:32 -0800

    updates main func

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+def load_data(file_path):
+    """
+    Loads the dataset from the specified file path.
+    """
+    df = pd.read_csv(file_path)
+    return df
 
 def time_of_day(hour):
     """
@@ -14,7 +20,7 @@ def time_of_day(hour):
         return "Evening/Night"
     
     
-def data_preprocess(df):
+def data_preprocess(file_path):
     """
     Preprocesses the input DataFrame by:
     - Filling missing values in 'NEIGHBOURHOOD' with 'Not Specified'
@@ -22,12 +28,18 @@ def data_preprocess(df):
     - Creating a new column 'TIME_OF_DAY' based on the 'HOUR' column
     - Renaming 'Central Business District' to 'Downtown' in the 'NEIGHBOURHOOD' column
     """
+    df = load_data(file_path)
+    
+    # replace missing NaN value with 'Fairview' in the 'neighbourhood'
+    df.loc[(df['HUNDRED_BLOCK'] == "15XX GRANVILLE BRDG") & 
+        (df['NEIGHBOURHOOD'].isna()) & (df['X'].notna()) & 
+        (df['Y'].notna()), 'NEIGHBOURHOOD'] = 'Fairview'
     
     # fill missing values in Neighbourhood with Not Specified
     df['NEIGHBOURHOOD'] = df['NEIGHBOURHOOD'].fillna('Not Specified')
 
     # create MONTH_NAME column
-    df['MONTH_NAME'] = pd.to_datetime(pd.to_numeric(df['MONTH'], errors='coerce'), format='%m', errors='coerce').dt.month_name()
+    df['MONTH_NAME'] = pd.to_datetime(df['MONTH'], format='%m', errors='coerce').dt.month_name()
 
     # apply time_of_day function to create column 'TIME_OF_DAY'
     df['TIME_OF_DAY'] = df['HOUR'].apply(time_of_day)
@@ -35,5 +47,11 @@ def data_preprocess(df):
     # rename Central Business District to Downtown
     df['NEIGHBOURHOOD'] = df['NEIGHBOURHOOD'].replace("Central Business District", "Downtown")
 
+    df.to_csv('data/processed/processed_vancouver_crime_data_2025.csv', index=False)
     
     return df
+
+
+if __name__ == "__main__":
+    df = data_preprocess('../data/raw/crimedata_csv_AllNeighbourhoods_2025.csv')
+    print('Data Preprocessing Completed')
