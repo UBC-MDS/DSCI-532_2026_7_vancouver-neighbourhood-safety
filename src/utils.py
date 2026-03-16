775c892f3b3f8f6104b75ebe9d4058816c55c313
Omar R <omar.ramos19@gmail.com>
2026-03-15 20:59:59 -0700

    Implement DuckDB in get_filtered_data function

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,62 @@
-def resolve_filter(values):
-        "Helper function to convert 'All' selections to None for easier filtering logic"
-        if not values or "All" in values:
-            return None
-        if isinstance(values, str):
-            return [values]
-        return values
+import duckdb
 
-def get_filtered_data(df, filter_nb=None, filter_crime=None, filter_month=None, filter_time=None):
-        """Helper function to apply selected filters to the vancouver neighbourhood data 
-        based on which filters are enabled"""
-        if filter_nb is not None:
-            df = df[df["NEIGHBOURHOOD"].isin(filter_nb)]
-                
-        if filter_crime is not None:
-            df = df[df["TYPE"].isin(filter_crime)]
-                
-        if filter_month is not None:
-            df = df[df["MONTH_NAME"].isin(filter_month)]
-                
-        if filter_time is not None:
-            df = df[df["TIME_OF_DAY"].isin(filter_time)]
-                
-        return df
+PARQUET_FILE = "data/processed/van_crime_data_2025.parquet"
+
+con = duckdb.connect()
+
+def resolve_filter(values):
+    "Helper function to convert 'All' selections to None "
+    "for easier filtering logic"
+
+    if not values or "All" in values:
+        return None
+    if isinstance(values, str):
+        return [values]
+    return values
+
+
+def get_filtered_data(
+    filter_nb=None, 
+    filter_crime=None, 
+    filter_month=None, 
+    filter_time=None
+):
+    """Helper function to apply selected filters to the vancouver 
+    neighbourhood data, based on which filters are enabled.
+    Parquet file + DuckDB + SQL approach."""
+
+    filter_nb = resolve_filter(filter_nb)
+    filter_crime = resolve_filter(filter_crime)
+    filter_month = resolve_filter(filter_month)
+    filter_time = resolve_filter(filter_time)
+
+    where_clause = []
+    params = []  # To bind values to query (for ? notation)
+
+    if filter_nb:
+        where_clause.append("NEIGHBOURHOOD IN (?)")
+        params.append(filter_nb)
+            
+    if filter_crime:
+        where_clause.append("TYPE IN (?)")
+        params.append(filter_crime)
+            
+    if filter_month:
+        where_clause.append("MONTH_NAME IN (?)")
+        params.append(filter_month)
+            
+    if filter_time:
+        where_clause.append("TIME_OF_DAY IN (?)")
+        params.append(filter_time)
+
+    sql = f"""
+        SELECT * 
+        FROM read_parquet('{PARQUET_FILE}')
+    """
+
+    if where_clause:
+        sql += " WHERE " + " AND ".join(where_clause)
+
+    df = con.execute(sql, params).df()
+            
+    return df
