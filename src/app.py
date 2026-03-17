9921ec7659eb57a1f647a62234b305e14a96fcd9
RANDALL Lee <randallxlee@gmail.com>
2026-03-16 23:56:15 -0700

    Updating app.py for import module

--- src/app.py
+++ src/app.py
@@ -14,7 +14,13 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 from datetime import datetime
 import os
-from utils import resolve_filter, get_filtered_data, get_neighbourhoods, get_crime_types
+import sys
+from pathlib import Path
+
+if __package__ is None or __package__ == "":
+    sys.path.append(str(Path(__file__).resolve().parent.parent))
+    __package__ = "src"
+from .utils import resolve_filter, get_filtered_data, get_neighbourhoods, get_crime_types
 
 load_dotenv()
 
